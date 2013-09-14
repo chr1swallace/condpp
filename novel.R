1cdc5e2186947ccf4affd7d99d8f51b026f04868
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-09-14 19:43:01 +0100

    updated to use Suna's results

--- novel.R
+++ novel.R
@@ -4,6 +4,18 @@ source("functions.R")
 ## NOVEL REGIONS
 regions <- read.table("novel_regions.csv",header=TRUE,sep=",",as.is=TRUE)
 
+## EXTRACT UVA RESULTS FOR THESE SNPS
+snps <- sub("imm_","",unique(regions$ic.id))
+cat(snps,file="newsnps.txt",sep="\n")
+
+system("zcat metamerged_ImmChip_CC_fam_MAF_09132013.txt.gz | head -n1 > newsnps.meta")
+system("zcat metamerged_ImmChip_CC_fam_MAF_09132013.txt.gz | grep -f newsnps.txt >> newsnps.meta")
+system("zcat SNPsToBeRemoved_from_metamerged.txt.gz | grep -f newsnps.txt > newsnps.rm")
+meta <- read.table("newsnps.meta",as.is=TRUE,header=TRUE)
+
+## not too dissimilar, but families do not provide support
+rownames(meta) <- as.character(meta$IMCHIP_Marker_ID)
+regions$t1d_uva <- meta[regions$ic.id, "P_CC"]
 
 ## PRIORS
 priors <- c(H1=1e-4,H2=1e-4,H12=1e-5)
@@ -21,11 +33,12 @@ N<-list( #case,control
 results <- vector("list",nrow(regions))
 for(i in 1:nrow(regions)) {
   d <- regions[i,"disease"]
+  cat(i,d,"\n")
   N0 <- min(N[[d]][2],N[["T1D"]][2])
   N1 <- N[[d]][1]
   N2 <- N[["T1D"]][1]
   p1 <- regions[i,"p.val"]
-  p2 <- regions[i,"t1d_cooper"]
+  p2 <- regions[i,"t1d_uva"]
   q0 <- regions[i,"MAF"]  
   results[[i]] <- runsim(N0=N0,N1=N1,N2=N2,q0=q0,p1=p1,p2=p2)
 }
